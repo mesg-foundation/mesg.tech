2a0fd5e7ea5e65d8b7fa65b90a6371bd11cd9c83
Will Morgan <38283291+w1l1ams@users.noreply.github.com>
2019-03-05 14:27:32 +0700

    Update Showcase.vue

--- components/Showcase.vue
+++ components/Showcase.vue
@@ -30,7 +30,7 @@
       </div>
     </div>
     
-    <h2>Who could benefit from this ?</h2>
+    <h2>Who could benefit from this?</h2>
 
     <nav mb2>
       <a v-for="(c, i) in showcase.companies" :key="i" :href="c.href" target="_blank">
