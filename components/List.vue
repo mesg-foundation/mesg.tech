b061bc98c1e751b09a84edb663224fb82ef338ce
Emmanuel Drouin <emmanuel@mesg.tech>
2019-11-19 14:48:56 +0700

    Update List component style css

--- components/List.vue
+++ components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <ul mb2>
-    <li mb1 v-for="(item, i) in items" :key="i">
+    <li mb2 v-for="(item, i) in items" :key="i" class="icon-list" flex align-center>
       <img mr1 v-if="item.src" :src="item.src" :alt="item.title" />
       {{ item.title }}
     </li>
@@ -19,13 +19,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-ul {
-  list-style: none;
+li {
+  min-height: 50px;
+  img {
+    height: 50px;
+    width: 50px;
+    vertical-align: middle;
+  }
 }
-
-li img {
-  height: 50px;
-  width: 50px;
-  vertical-align: middle;
+.icon-list {
+  padding-left: calc(var(--margin) * 4);
 }
 </style>
